e454126938d946c624eac51a4e9c2e931b3db9a0
manthan-web <gamingmn1102@gmail.com>
2024-02-04 17:08:41 +0530

    added light and dark mode

--- script.js
+++ script.js
@@ -63,6 +63,60 @@ function changingWeather (weatherCondition) {
 
 
 
+// light and dark mode togglelling
+
+
+// Icons
+const sunIcon = document.querySelector('.light-mode')
+const moonIcon = document.querySelector('.dark-mode')
+
+// Theme Vars
+const userTheme = localStorage.getItem("theme")
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+
+// Icon Toggling
+const iconToggle = () => {
+    sunIcon.classList.toggle("display-none")
+    moonIcon.classList.toggle("display-none")
+}
+
+// Intial Theme Check
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark")
+        moonIcon.classList.add("display-none")
+        return
+    }
+    sunIcon.classList.add("display-none")
+}
+
+// manual theme swtich
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light")
+        iconToggle()
+        return
+    }
+    document.documentElement.classList.add("dark")
+    localStorage.setItem("theme", "dark")
+    iconToggle()
+}
+
+// call theme switch on clicking buttons
+sunIcon.addEventListener("click", () => {
+    themeSwitch();
+})
+
+moonIcon.addEventListener("click", () => {
+    themeSwitch();
+})
+
+// invoke theme check on intial load
+themeCheck();
+
+
 
 
 
